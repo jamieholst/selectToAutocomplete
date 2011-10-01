47344133d957ef412b36390a3be2d94082572fed
Jamie Appleseed <jamie@gmin.org>
2011-10-01 20:21:45 +0200

    Initial version with jQuery UI adapter

---
+++ jquery.select-to-autocomplete.js
@@ -0,0 +1,203 @@
+/*
+Copyright (C) 2011 by Jamie Appleseed at Baymard Institute
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+(function($){
+  var settings = {
+    'sort': false,
+    'sort-attr': 'data-priority',
+    'sort-desc': false,
+    'alternative-spellings': true,
+    'alternative-spellings-attr': 'data-alternative-spellings',
+    'remove-valueless-options': true,
+    'copy-attributes-to-text-field': true,
+    'autocomplete-plugin': 'jquery_ui',
+    'clear-invalid-input': true,
+    handle_select_field: function( $select_field ) {
+      return $select_field.hide();
+    },
+    insert_text_field: function( $select_field ) {
+      var $text_field = $( "<input></input>" );
+      if ( settings['copy-attributes-to-text-field'] ) {
+        var attrs = {};
+        var raw_attrs = $select_field[0].attributes;
+        for (var i=0; i < raw_attrs.length; i++) {
+          var key = raw_attrs[i].nodeName;
+          var value = raw_attrs[i].nodeValue;
+          if ( key !== 'name' && key !== 'id' ) {
+            attrs[key] = raw_attrs[i].nodeValue;
+          }
+        };
+        return $text_field.attr( attrs )
+          .val( $select_field.find('option:selected:first').text() )
+          .insertAfter( $select_field );
+      }
+    },
+    extract_options: function( $select_field ) {
+      var options = [];
+      var $options = $select_field.find('option');
+      var number_of_options = $options.length;
+      var original_number_of_options = number_of_options;
+      
+      // go over each option in the select tag
+      $options.each(function(){
+        var $option = $(this);
+        var option = {
+          'real-value': $option.attr('value'),
+          'label': $option.text()
+        }
+        if ( settings['remove-valueless-options'] && option['real-value'] === '') {
+          // remove options without a value
+          number_of_options--;
+        } else {
+          // prepare the 'matches' string which must be filtered on later
+          option['matches'] = option['label'];
+          var alternative_spellings = $option.attr( settings['alternative-spellings-attr'] );
+          if ( alternative_spellings ) {
+            option['matches'] += ' ' + alternative_spellings;
+          }
+          // give each option a weight paramter for sorting
+          if ( settings['sort'] ) {
+            var weight = parseInt( $option.attr( settings['sort-attr'] ), 10 );
+            if ( weight ) {
+              option['weight'] = weight;
+            } else {
+              option['weight'] = original_number_of_options;
+            }
+          }
+          // add option to combined array
+          options.push( option );
+        }
+      });
+      // sort the options based on weight
+      if ( settings['sort'] ) {
+        if ( settings['sort-desc'] ) {
+          options.sort( function( a, b ) { return b['weight'] - a['weight']; } );
+        } else {
+          options.sort( function( a, b ) { return a['weight'] - b['weight']; } );
+        }
+      }
+      
+      // return the set of options, each with the following attributes: real-value, label, matches, weight (optional)
+      return options;
+    }
+  };
+  
+  var public_methods = {
+    init: function( customizations ) {
+      settings = $.extend( settings, customizations );
+      
+      return this.each(function(){
+        var $select_field = $(this);
+        
+        var options = settings['extract_options']( $select_field );
+        var $text_field = settings['insert_text_field']( $select_field );
+        settings['handle_select_field']( $select_field );
+        
+        var context = {
+          '$select_field': $select_field,
+          '$text_field': $text_field,
+          'options': options,
+          'settings': settings
+        };
+        if ( typeof settings['autocomplete-plugin'] === 'string' ) {
+          adapters[settings['autocomplete-plugin']]( context );
+        } else {
+          settings['autocomplete-plugin']( context );
+        }
+      });
+    }
+  };
+  
+  var adapters = {
+    jquery_ui: function( context ) {
+      // loose matching of search terms
+      var filter_options = function( term ) {
+        var split_term = term.split(' ');
+        var matchers = [];
+        for (var i=0; i < split_term.length; i++) {
+				  if ( split_term[i].length > 0 ) {
+				    matchers.push( new RegExp( $.ui.autocomplete.escapeRegex( split_term[i] ), "i" ) );
+				  }
+				};
+				
+				return $.grep( context.options, function( option ) {
+  				var partial_matches = 0;
+  				for ( var i=0; i < matchers.length; i++ ) {
+  				  if ( matchers[i].test( option.matches ) ) {
+  				    partial_matches++;
+  				  }
+  				};
+  				return (!term || matchers.length === partial_matches );
+				});
+      }
+      // update the select field value using either selected option or current input in the text field
+      var update_select_value = function( option ) {
+        if ( option ) {
+          context.$select_field.val( option['real-value'] );
+        } else {
+          var option_name = context.$text_field.val();
+          var option_value = '';
+          for (var i=0; i < context.options.length; i++) {
+            if ( option_name === context.options[i]['label'] ) {
+              option_value = context.options[i]['real-value'];
+              break;
+            }
+          };
+          context.$select_field.val( option_value );
+  		    if ( context.settings['clear-invalid-input'] && context.$select_field.val() === '' ) {
+  		      context.$text_field.val( '' );
+  		    }
+        }
+      }
+      // jQuery UI autocomplete settings & behavior
+      context.$text_field.autocomplete({
+        'minLength': 0,
+        'delay': 0,
+        source: function( request, response ) {
+          response( filter_options( request.term ) );
+        },
+        select: function( event, ui ) {
+          update_select_value( ui.item );
+				},
+				change: function( event, ui ) {
+				  update_select_value( ui.item );
+				}
+      });
+      // force refresh value of select field when form is submitted
+      context.$text_field.parents('form:first').submit(function(){
+        update_select_value();
+      });
+      // select current value
+      update_select_value();
+    }
+  };
+
+  $.fn.selectToAutocomplete = function( method ) {
+    if ( public_methods[method] ) {
+      return public_methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return public_methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+    }    
+  };
+  
+})(jQuery);
